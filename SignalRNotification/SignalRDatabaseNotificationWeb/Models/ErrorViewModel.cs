f51530dbc9f2c5336bd40095a6e9d47ef818a8d4
dong.zhao <dzhaoau@gmail.com>
2022-11-20 15:08:08 +1100

    delete useless project

--- SignalRNotification/SignalRDatabaseNotificationWeb/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace SignalRDatabaseNotificationWeb.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
