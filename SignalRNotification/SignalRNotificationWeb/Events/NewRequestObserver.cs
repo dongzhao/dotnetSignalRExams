dec54d5e85e583678d0cd3deb5d8e2da8e9e3663
dong.zhao <dzhaoau@gmail.com>
2022-11-20 15:51:48 +1100

    add Observer pattern using event to call signalR

---
+++ SignalRNotification/SignalRNotificationWeb/Events/NewRequestObserver.cs
@@ -0,0 +1,25 @@
+﻿namespace SignalRNotificationWeb.Events
+{
+    /// <summary>
+    /// Observer pattern using event: An observer class 
+    /// </summary>
+    public class NewRequestObserver
+    {
+        NewRequestSubject subject;
+
+        public NewRequestObserver(NewRequestSubject sub)
+        {
+            this.subject = sub;
+        }
+
+        public void Subscribe()
+        {
+            subject.eventHandler += SendToHub;
+        }
+
+        private void SendToHub(object sender, EventArgs e)
+        {
+            Console.WriteLine("calling signal....");
+        }
+    }
+}
---
+++ SignalRNotification/SignalRNotificationWeb/Events/NewRequestSubject.cs
@@ -0,0 +1,19 @@
+﻿namespace SignalRNotificationWeb.Events
+{
+    /// <summary>
+    /// Event pattern subject class follow subject/observer
+    /// </summary>
+    public class NewRequestSubject
+    {
+        public event EventHandler eventHandler;
+
+        public void NotifyObservers()
+        {
+            if(eventHandler != null)
+            {
+                eventHandler(this, EventArgs.Empty);
+            } 
+        }
+
+    }
+}
